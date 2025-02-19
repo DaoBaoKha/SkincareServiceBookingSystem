f12fa40a55a3e8e3b98822f2cead0e71b689a816
DuongDucManh <manhmanhd0402@gmail.com>
2025-02-19 15:57:50 +0700

    Changing generic repo to use queriable

--- SkincareBookingService/SkincareBookingService.DAL/Interfaces/IGenericRepository.cs
+++ SkincareBookingService/SkincareBookingService.DAL/Interfaces/IGenericRepository.cs
@@ -4,6 +4,7 @@ namespace SkincareBookingService.DAL.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
+        IQueryable<T> Query();
         Task<T?> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
--- SkincareBookingService/SkincareBookingService.DAL/Repositories/GenericRepository.cs
+++ SkincareBookingService/SkincareBookingService.DAL/Repositories/GenericRepository.cs
@@ -8,12 +8,17 @@ namespace SkincareBookingService.DAL.Repositories
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         protected readonly SkincareBookingSystemContext _context;
+        private readonly DbSet<T> _dbSet;
 
         public GenericRepository(SkincareBookingSystemContext context)
         {
             _context = context;
+            _dbSet = context.Set<T>();
         }
-
+        public IQueryable<T> Query()
+        {
+            return _dbSet.AsQueryable(); // Expose IQueryable
+        }
         public async Task<T?> GetByIdAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id);
